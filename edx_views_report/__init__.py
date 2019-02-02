db75505eec5d25714e8492d74e8fac84ecdc3daa
MasterGowen <mastergowen@gmail.com>
2019-02-02 19:14:21 +0500

    init#5

--- edx_views_report/__init__.py
+++ edx_views_report/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 default_app_config = 'edx_views_report.apps.EdxViewsReportConfig'  # pylint: disable=invalid-name
